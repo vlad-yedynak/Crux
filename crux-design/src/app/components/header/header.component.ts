f59c0c87b9f3d85852798e6fa05595155cc6c77f
Yurii Surniak <surniakyura@gmail.com>
2025-04-01 18:18:17 +0300

    Refactor header component: improve styles, add dropdown functionality, and separate template

--- crux-design/src/app/components/header/header.component.ts
+++ crux-design/src/app/components/header/header.component.ts
@@ -1,28 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
-  template: `
-    <header class="main-header">
-        <div class="logo">
-            <h1>Goida</h1>
-        </div>
-        <div class="profile-container">
-            <div class="profile">
-                <img src="./profile_placeholder.jpg" alt="Profile" class="profile-pic">
-                <span class="profile-name">User Name</span>
-                <div class="profile-dropdown">
-                    <button class="btn sign-in">Sign In</button>
-                    <button class="btn sign-up">Sign Up</button>
-                </div>
-            </div>
-        </div>
-    </header>
-  `,
-  styleUrl: './header.component.css'
+  standalone: true,
+  imports: [CommonModule], 
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent{
+    isDropdownVisible: boolean = false;
 
+    toggleDropdown(): void {
+        this.isDropdownVisible = !this.isDropdownVisible;
+      }
 }
