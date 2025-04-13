af04ba3e30ffe9f985b48ede45c48882ab8cbc74
Keerill220 <kponomarenko161@gmail.com>
2025-04-13 14:10:40 +0300

    Added Signup form validations

--- crux-design/src/app/auth/signup-form/signup-form.component.ts
+++ crux-design/src/app/auth/signup-form/signup-form.component.ts
@@ -1,10 +1,13 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { first } from 'rxjs';
+import e from 'express';
 
 @Component({
   selector: 'app-signup-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signup-form.component.html',
   styleUrl: './signup-form.component.css'
 })
@@ -14,4 +17,44 @@ export class SignupFormComponent {
   onSwitchToLogin() {
     this.switchToLogin.emit();
   }
+
+  signupForm: any;
+
+  constructor(public formBuilder:FormBuilder){
+    this.signupForm = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: [''],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { 
+      validators: this.passwordMatch 
+    });
+  }
+
+  passwordMatch: ValidatorFn = (control: AbstractControl): null | { [key: string]: boolean } => {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    
+    if (password && confirmPassword && password.value !== confirmPassword.value) {
+      confirmPassword.setErrors({...confirmPassword.errors, passwordMismatch: true});
+      return { passwordMismatch: true };
+    } else if (confirmPassword?.errors?.['passwordMismatch']) {
+      const errors = {...confirmPassword.errors};
+      delete errors['passwordMismatch'];
+      confirmPassword.setErrors(Object.keys(errors).length ? errors : null);
+    }
+    
+    return null;
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+      //todo: Signup logic
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
 }
